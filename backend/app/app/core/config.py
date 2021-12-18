2d66ec70740ba63a4cf5555b8570384b88ee1488
alexander <bf-109g@yandex.ru>
2021-12-18 20:32:09 +0300

    Deployment updated

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -10,11 +10,12 @@ class Settings(BaseSettings):
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_NAME: str = "medical"
-    SERVER_HOST: AnyHttpUrl = "http://localhost"
+    SERVER_HOST: AnyHttpUrl = "http://127.0.0.1"
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = ['http://127.0.0.1:8989', 'http://127.0.0.1:81',
+                                              'http://localhost:81', 'http://localhost:8989']
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
@@ -72,8 +73,8 @@ class Settings(BaseSettings):
 
     EMAIL_TEST_USER: EmailStr = "test@example.com"  # type: ignore
     FIRST_SUPERUSER: EmailStr = "admin@hc-solutions.com"
-    FIRST_SUPERUSER_PASSWORD: str = "change_me_in_production"
-    USERS_OPEN_REGISTRATION: bool = False
+    FIRST_SUPERUSER_PASSWORD: str = "qwerty"
+    USERS_OPEN_REGISTRATION: bool = True
 
     class Config:
         case_sensitive = True
