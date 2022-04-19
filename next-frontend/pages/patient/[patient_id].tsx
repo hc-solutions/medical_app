62c01940b67a84899a5e4164a7f881fbe7c8b9f3
alexander <bf-109g@yandex.ru>
2022-04-19 19:46:31 +0300

    Update README.md, docker-compose.yml
    Roadmap was added, RabbitMQ was deleted from docker-compose

--- next-frontend/pages/patient/[patient_id].tsx
+++
@@ -1,8 +0,0 @@
-import { useRouter } from "next/router";
-
-export default () => {
-    const router = useRouter();
-
-    const { patient_id } = router.query;
-    return <div>Patient id: {patient_id}</div>
-}
